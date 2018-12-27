36726d7ec1016ba18c8ae51b06710c8bbeda0a75
mbj36 <mohitbajo36@gmail.com>
2018-12-27 16:14:26 +0530

    feat: basic function added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,15 @@
     <div>
         <h1 class="__header_title">Finding Falcone!</h1>
         <h2 class="__select_planet">Select planets you want to search in - </h2>
-        <v-flex xs12 sm9 d-flex justify-center>
-            <v-select :items="getPlanetName" label="Select" outline></v-select>
-            <v-select :items="getPlanetName" label="Select" outline></v-select>
-            <v-select :items="getPlanetName" label="Select" outline></v-select>
-            <v-select :items="getPlanetName" label="Select" outline></v-select>
+        <v-flex d-flex xs12 sm7 mt-5 class="__select_destination">
+            <v-select :items="items" label="Select Destination 1" outline></v-select>
+            <v-select :items="items" label="Select Destination 2" outline></v-select>
+            <v-select :items="items" label="Select Destination 3" outline></v-select>
+            <v-select :items="items" label="Select Destination 4" outline></v-select>
         </v-flex>
+        <div class="__find_falcon">
+            <v-btn>Find Falcon</v-btn>
+        </div>
     </div>
 </template>
 
@@ -15,24 +18,63 @@
     export default {
       data() {
         return {
-          items: []
+          items: [],
+          vehicles: [],
+          token: null,
+          selected_planet_names: [],
+          selected_vehicle_names: []
         };
       },
 
       mounted() {
         this.getPlanets();
+        this.getToken();
       },
 
       methods: {
         getPlanets() {
           this.$http.get('https://findfalcone.herokuapp.com/planets').then(res => {
-            this.items = res.data;
+            this.items = res.data.map(data => data.name);
           });
+        },
+
+        getVehicles() {
+          this.$http.get('https://findfalcone.herokuapp.com/vehicles').then(res => {
+            this.vehicles = res.data;
+          });
+        },
+
+        getToken() {
+          this.$http
+            .post(
+              'https://findfalcone.herokuapp.com/token',
+              {},
+              {
+                headers: {
+                  Accept: 'application/json'
+                }
+              }
+            )
+            .then(res => {
+              this.token = res.data.token;
+            });
+        },
+
+        findFalcon() {
+          this.$http
+            .post('https://findfalcone.herokuapp.com/find', {
+              token: this.token,
+              planet_names: this.selected_planet_names,
+              vehicle_names: this.selected_vehicle_names
+            })
+            .then(res => {
+              alert('Thanks');
+            });
         }
       },
       computed: {
         getPlanetName() {
-          return this.items.map(item => item.name);
+          return this.items;
         }
       }
     };
@@ -48,5 +90,17 @@
     .__select_planet {
       text-align: center;
       margin-top: 1rem;
+    }
+
+    .__select_destination {
+      margin-left: 20%;
+    }
+
+    .__select_destination > div {
+      margin-left: 2% !important;
+    }
+    .__find_falcon {
+      margin-top: 10%;
+      text-align: center;
     }
 </style>
