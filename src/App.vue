3cb923b0f2e81ab0726659ade33ae04103146d21
mbj36 <mohitbajo36@gmail.com>
2018-12-31 19:17:12 +0530

    fix: show the planet name above vehicles name

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,70 @@
 <template>
-    <v-app>
-        <div id="app">
-            <Header/>
-            <!-- View -->
-            <router-view/>
-        </div>
-        <Footer/>
-    </v-app>
+  <v-app>
+    <div id="app">
+      <Header/>
+      <!-- View -->
+      <router-view/>
+    </div>
+    <Footer/>
+  </v-app>
 </template>
 
 <script>
-    import Header from './components/Header'; // Header Component
-    import Footer from './components/Footer'; // Footer Component
-    export default {
-      name: 'app',
-      components: {
-        Header,
-        Footer
-      }
-    };
+  import Header from './components/Header'; // Header Component
+  import Footer from './components/Footer'; // Footer Component
+  export default {
+    name: 'app',
+    components: {
+      Header,
+      Footer
+    }
+  };
 </script>
 
 <style>
-    #app {
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      color: #2c3e50;
-    }
-    .footer {
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      padding: 1rem;
-      background-color: #efefef;
-      text-align: center;
-    }
-    .__header_title {
-      font-size: 3rem;
-      text-align: center;
-      margin-top: 3rem;
-    }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  .footer {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 1rem;
+    background-color: #efefef;
+    text-align: center;
+  }
+  .__header_title {
+    font-size: 3rem;
+    text-align: center;
+    margin-top: 3rem;
+  }
 
-    .__select_planet {
-      text-align: center;
-      margin-top: 1rem;
-    }
+  .__select_planet {
+    text-align: center;
+    margin-top: 1rem;
+  }
 
-    .__find_falcon {
-      margin-top: 3%;
-      text-align: center;
-    }
+  .__find_falcon {
+    margin-top: 3%;
+    text-align: center;
+  }
+  .__vehicles_list {
+    display: inline-flex;
+  }
+  .__vehicles_list > div {
+    justify-content: space-between;
+  }
+  @media screen and (max-width: 40em) {
     .__vehicles_list {
-      display: inline-flex;
-      margin-left: 5%;
-      width: 65%;
+      width: 100%;
+      margin-left: 0%;
     }
-    .__vehicles_list > div {
-      justify-content: space-between;
-    }
-    @media screen and (max-width: 40em) {
-      .__vehicles_list {
-        width: 100%;
-        margin-left: 0%;
-      }
-    }
-    .__show_destination {
-      text-align: center;
-    }
+  }
+  .__show_destination {
+    text-align: center;
+  }
 </style>
--- src/components/Vehicles.vue
+++ src/components/Vehicles.vue
@@ -3,28 +3,37 @@
         <p class="text-sm-center mt-3" v-if="selectedPlanetNames.length > 3">
             Now Select the vehicles for each planet...you can't change the planet names now but you can always start fresh by resetting
         </p>
+
         <div class="__vehicles_list" v-if="selectedPlanetNames.length > 3">
 
             <v-radio-group v-model="form.radio[0]">
-
+                <p>
+                    <b>{{selectedPlanetNames[0]}}</b>
+                </p>
                 <v-radio v-for="(vehicle, index) in vehicles" :key="index" @change="timeTaken(vehicle.max_distance, vehicle.speed, 1)" :disabled="vehicle.max_distance < selectedPlanet[0].distance" :label="`${vehicle.name} (${vehicle.total_no})`" :value="vehicle.name"></v-radio>
 
             </v-radio-group>
 
             <v-radio-group v-model="form.radio[1]">
-
+                <p>
+                    <b>{{selectedPlanetNames[1]}}</b>
+                </p>
                 <v-radio v-for="(vehicle, index) in vehicles" :key="index" @change="timeTaken(vehicle.max_distance, vehicle.speed, 2)" :disabled="vehicle.max_distance < selectedPlanet[1].distance" :label="`${vehicle.name} (${vehicle.total_no})`" :value="vehicle.name"></v-radio>
 
             </v-radio-group>
 
             <v-radio-group v-model="form.radio[2]">
-
+                <p>
+                    <b>{{selectedPlanetNames[2]}}</b>
+                </p>
                 <v-radio v-for="(vehicle, index) in vehicles" :key="index" @change="timeTaken(vehicle.max_distance, vehicle.speed, 3)" :disabled="vehicle.max_distance < selectedPlanet[2].distance" :label="`${vehicle.name} (${vehicle.total_no})`" :value="vehicle.name"></v-radio>
 
             </v-radio-group>
 
             <v-radio-group v-model="form.radio[3]">
-
+                <p>
+                    <b>{{selectedPlanetNames[3]}}</b>
+                </p>
                 <v-radio v-for="(vehicle, index) in vehicles" :key="index" @change="timeTaken(vehicle.max_distance, vehicle.speed, 4)" :disabled="vehicle.max_distance < selectedPlanet[3].distance" :label="`${vehicle.name} (${vehicle.total_no})`" :value="vehicle.name"></v-radio>
 
             </v-radio-group>
